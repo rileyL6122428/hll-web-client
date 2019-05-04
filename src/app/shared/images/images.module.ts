f94e9452d8ef7d7a3d13843590af163bcabbcfce
Riley Littlefield <rileylittlefield@ymail.com>
2019-05-04 00:46:36 -0400

    continues profile page

--- src/app/shared/images/images.module.ts
+++ src/app/shared/images/images.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { OcarinaLinkComponent } from './ocarina-link/ocarina-link.component';
+import { PlayButtonComponent } from './play-button/play-button.component';
 
 @NgModule({
   declarations: [
     OcarinaLinkComponent,
+    PlayButtonComponent,
   ],
   exports: [
-    OcarinaLinkComponent
+    OcarinaLinkComponent,
+    PlayButtonComponent,
   ]
 })
 export class ImagesModule { }
