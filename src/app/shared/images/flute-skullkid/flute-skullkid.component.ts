41c021fe23c2a844d68217818eb8199272cedb04
Riley Littlefield <rileylittlefield@ymail.com>
2019-05-13 23:16:06 -0400

    deletes dead code

--- src/app/shared/images/flute-skullkid/flute-skullkid.component.ts
+++ src/app/shared/images/flute-skullkid/flute-skullkid.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'hll-flute-skullkid',
   templateUrl: './flute-skullkid.component.html',
   styleUrls: ['./flute-skullkid.component.scss']
 })
-export class FluteSkullkidComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class FluteSkullkidComponent { }
