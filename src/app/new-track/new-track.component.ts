768c4f157fe8527293ffed2f5304163dd87fb088
Riley Littlefield <rileylittlefield@ymail.com>
2019-07-07 21:27:47 -0400

    finishes connection test

--- src/app/new-track/new-track.component.ts
+++ src/app/new-track/new-track.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../shared/auth/auth.service';
 
 @Component({
   selector: 'hll-new-track',
@@ -8,11 +9,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class NewTrackComponent implements OnInit {
 
-  audioFile: File;
   @ViewChild('fileInput') fileInputElement: { nativeElement: HTMLInputElement };
+  private trackName = '';
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
@@ -20,15 +22,25 @@ export class NewTrackComponent implements OnInit {
 
   submit(): void {
     const formData = new FormData();
-    formData.append('audio-file', this.fileInputElement.nativeElement.files[0], 'TESTING');
+    formData.append('audio-file', this.fileInputElement.nativeElement.files[0], this.trackName);
 
     this.httpClient.post(
-      'http://localhost:8080/api/public/audio/upload',
-      formData
+      'http://localhost:8080/api/private/track',
+      formData,
+      {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${this.authService.idToken}`
+        })
+      }
     )
-      .subscribe((response) => {
-        console.log('response', response);
-      });
+      .subscribe(
+        (response) => {
+          console.log('response', response);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   }
 
 }
