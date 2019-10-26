71da38fd12e8e10d669d7de4f9f27012ca675d8f
Riley Littlefield <rileylittlefield@ymail.com>
2019-10-26 17:44:26 -0400

    fixes comp error in prod build

--- src/app/api-client-config.module.ts
+++ src/app/api-client-config.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-// import { trackClientConfigToken, TrackClientConfig } from './shared/http-clients/track.http';
 import { trackClientConfigToken, TrackClientConfig } from 'hll-shared-client';
 import { environment } from 'src/environments/environment';
 
@@ -11,7 +10,9 @@ import { environment } from 'src/environments/environment';
         urls: {
           upload: 'http://localhost:8080/api/private/track',
           getAllForUser: environment.API.TRACKS.GET_ALL_FOR_USER,
-          delete: (trackId: string) => `http://localhost:8080/api/private/track/${trackId}`
+          delete(trackId: string) {
+            return `http://localhost:8080/api/private/track/${trackId}`;
+          }
         }
       } as TrackClientConfig
     }
